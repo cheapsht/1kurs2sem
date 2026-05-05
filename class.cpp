4b509feb3c8a029cdd230ca8f58c07a72603dc40
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 16:45:05 +0300

    class.cpp

--- class.cpp
+++ class.cpp
@@ -30,6 +30,37 @@ Point3D::Point3D(Point3D&& other) noexcept
 double Point3D::getX() const { return x; }
 double Point3D::getY() const { return y; }
 double Point3D::getZ() const { return z; }
+
+/**
+ * @brief копирующий оператор присваивания
+ * @param other другая точка, которую копируем
+ * @return ссылка на текущий объект
+ */
+Point3D& Point3D::operator=(const Point3D& other)
+{
+    if (this != &other) {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+    }
+    return *this;
+}
+
+/**
+ * @brief перемещающий оператор присваивания
+ * @param other временная точка
+ * @return ссылка на текущий объект
+ */
+Point3D& Point3D::operator=(Point3D&& other) noexcept
+{
+    if (this != &other) {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+    }
+    return *this;
+}
+
 /**
  * @brief оператор вычитания двух точек
  * @param other точка, вычитаемая из текущей
@@ -39,6 +70,12 @@ Point3D Point3D::operator-(const Point3D& other) const
 {
     return Point3D(other.x -x, other.y-y, other.z - z);
 }
+/**
+ * @brief вычисляет евклидово расстояние между двумя точками в трёхмерном пространстве
+ * @param A первая точка
+ * @param B вторая точка
+ * @return расстояние между точками A и B
+ */
 double Point3D::distance(const Point3D& A, const Point3D& B)
 {
     Point3D vec = A - B;
