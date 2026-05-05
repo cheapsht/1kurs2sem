b1879326da559e672e4b0635911c240c01b2c51c
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 17:13:28 +0300

    main.cpp

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "class.h"
+#include "tetrahedron.h"
+/**
+* @brief точка входа в программу
+* @return 0, если программа выполнена успешно
+*/
+int main() {
+    Point3D A(1.0, 1.0, 1.0);
+    Point3D B(1.0, -1.0, -1.0);
+    Point3D C(-1.0, 1.0, -1.0);
+    Point3D D(-1.0, -1.0, 1.0);
 
+    Tetrahedron t(A, B, C, D);
+
+    std::cout << "Площадь основания: " << t.getBaseArea() << std::endl;
+
+    return 0;
+}
