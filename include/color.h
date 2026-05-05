40ce38ef393664e617662b7b5c570eceeca87180
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 15:23:17 +0300

    color.h

---
+++ include/color.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <cstdint>
+#include <stdexcept>
+
+namespace graphics {
+    /** @brief Класс Цвет, хранит компоненты RGB в диапазоне [0, 255]. */
+    class Color {
+    private:
+        uint8_t r, g, b;
+        void validate(int r, int g, int b) const;
+    public:
+        /**
+         * @brief Конструктор цвета.
+         */
+        Color(int r = 0, int g = 0, int b = 0);
+
+        uint8_t getR() const;
+        uint8_t getG() const;
+        uint8_t getB() const;
+
+        friend std::ostream& operator<<(std::ostream& os, const Color& c);
+        friend std::istream& operator>>(std::istream& is, Color& c);
+    };
+}
