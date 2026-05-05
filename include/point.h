595e6b185f934996fcac671d51f318ed1dce6a14
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 15:24:20 +0300

    point.h

---
+++ include/point.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+namespace graphics {
+    /** @brief Структура Точка, содержащая абсциссу и ординату. */
+    struct Point {
+        double x;
+        double y;
+
+        /** @brief Конструктор точки. */
+        Point(double x = 0.0, double y = 0.0);
+    };
+
+    /** @brief Оператор вывода точки в поток. */
+    std::ostream& operator<<(std::ostream& os, const Point& p);
+    /** @brief Оператор ввода точки из потока. */
+    std::istream& operator>>(std::istream& is, Point& p);
+    /** @brief Оператор сравнения точек на равенство. */
+    bool operator==(const Point& lhs, const Point& rhs);
+}
