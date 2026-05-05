f03669df5f03956dae612157da4584faa274a309
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 16:55:33 +0300

    point.h

---
+++ task1/point.h
@@ -0,0 +1,76 @@
+#pragma once
+/**
+ * @class Point3D
+ * @brief Класс, описывающий  точку в 3-мерном пространстве тетраэдра,
+ * хранит координаты x, y, z и предоставляет базовые операции
+ * для работы с точками.
+ */
+class Point3D
+{
+private:
+/**
+* @brief x координата X
+* @brief  y координата Y
+* @brief z координата Z
+*/
+double x,y,z;
+
+public:
+/**
+* @brief Конструктор по умолчанию / с параметрами
+* @param x Координата X (по умолчанию 0)
+* @param y Координата Y (по умолчанию 0)
+* @param z Координата Z (по умолчанию 0)
+*/
+Point3D(double x = 0, double y = 0, double z = 0);
+/**
+* @brief копирующий конструктор
+* @param other другая точка, которую копируем
+*/
+Point3D(const Point3D& other);
+/**
+* @brief перемещающий конструктор
+* @param other временная точка
+*/
+Point3D(Point3D&& other) noexcept;
+/**
+* @brief возвращает координату x
+* @return значение координаты x
+*/
+double getX() const;
+/**
+* @brief возвращает координату y
+* @return значение координаты y
+*/
+double getY() const;
+/**
+* @brief возвращает координату z
+* @return значение координаты z
+*/
+double getZ() const;
+/**
+* @brief копирующий оператор присваивания
+* @param other другая точка, которую копируем
+* @return ссылка на текущий объект
+*/
+Point3D& operator=(const Point3D& other);
+/**
+* @brief перемещающий оператор присваивания
+* @param other временная точка
+* @return ссылка на текущий объект
+*/
+Point3D& operator=(Point3D&& other) noexcept;
+/**
+* @brief возвращает вектор от текущей точки к `other` (this → other).
+* @param otherк коонечная точка вектора
+* @return вектор между точками
+*/
+Point3D operator-(const Point3D& other) const;
+/**
+* @brief вычисляет евклидово расстояние между двумя точками в трёхмерном пространстве
+* @param A первая точка
+* @param B вторая точка
+* @return расстояние между точками A и B
+*/
+static double distance(const Point3D& A, const Point3D& B);
+};
