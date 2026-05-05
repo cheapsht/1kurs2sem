3cbfe9f5e0699e01b174f0f6fb24324f0530ae04
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 17:14:26 +0300

    point.h

--- task1/point.h
+++ task1/point.h
@@ -22,7 +22,7 @@ public:
 * @param y Координата Y (по умолчанию 0)
 * @param z Координата Z (по умолчанию 0)
 */
-Point3D(double x = 0, double y = 0, double z = 0);
+Point3D(const double x = 0,const double y = 0,const  double z = 0);
 /**
 * @brief копирующий конструктор
 * @param other другая точка, которую копируем
