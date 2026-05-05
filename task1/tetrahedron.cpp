1c46ca8f150c426f9c81b377fce9ffb6fd69faff
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-05 17:33:42 +0300

    tetrahedron.cpp

--- task1/tetrahedron.cpp
+++ task1/tetrahedron.cpp
@@ -1,7 +1,24 @@
 #include "tetrahedron.h"
 #include <cmath>
 #include <utility>
+#include <cfloat>
+/**
+ * @brief Проверяет, образуют ли 4 точки действительный тетраэдр
+ * @return true если точки не компланарны (образуют тетраэдр), false иначе
+ */
+bool Tetrahedron::isValid() const {
+    Point3D vecAB = B - A;
+    Point3D vecAC = C - A;
+    Point3D vecAD = D - A;
 
+    // Вычисляем смешанное произведение векторов (скалярное произведение AD на векторное произведение AB и AC)
+    // Если смешанное произведение равно 0, то точки компланарны
+    double crossX = vecAB.getY() * vecAC.getZ() - vecAB.getZ() * vecAC.getY();
+    double crossY = vecAB.getZ() * vecAC.getX() - vecAB.getX() * vecAC.getZ();
+    double crossZ = vecAB.getX() * vecAC.getY() - vecAB.getY() * vecAC.getX();
+    double scalarTripleProduct = vecAD.getX() * crossX + vecAD.getY() * crossY + vecAD.getZ() * crossZ;
+    return std::abs(scalarTripleProduct) > DBL_EPSILON;
+}
 /**
  * @brief Конструктор, инициализирующий тетраэдр заданными точками
  * @param a точка A
@@ -9,10 +26,13 @@
  * @param c точка C
  * @param d точка D
  */
-Tetrahedron::Tetrahedron(const double  Point3D& a, const double Point3D& b,
-                         const double Point3D& c, const double Point3D& d)
+Tetrahedron::Tetrahedron(const Point3D& a, const Point3D& b,
+                         const Point3D& c, const Point3D& d)
     : A(a), B(b), C(c), D(d)
 {
+    if (!isValid()) {
+        throw std::invalid_argument("точки не образуют тетрайдер,тк они компланарны");
+    }
 }
 
 /**
@@ -46,6 +66,9 @@ Tetrahedron& Tetrahedron::operator=(const Tetrahedron& other)
         B = other.B;
         C = other.C;
         D = other.D;
+      if (!isValid()) {
+            throw std::invalid_argument("точки не образуют тетрайдер,тк они компланарны");
+        }
     }
     return *this;
 }
