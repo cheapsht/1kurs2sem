8c2d1b0da83ee4491739a64f1f3b0c9eaf665914
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-12 18:41:54 +0300

    figure.h

--- include/figure.h
+++ include/figure.h
@@ -3,10 +3,20 @@
 #include <string>
 
 namespace graphics {
+     /** @brief Абстрактный базовый класс Фигура. */
     class Figure {
     public:
+        /** @brief Виртуальный деструктор. */
         virtual ~Figure() = default;
+        /**
+         * @brief Сериализация объекта в строку.
+         * @return Строковое представление фигуры.
+         */
         virtual std::string ToString() const = 0;
+        /**
+         * @brief Чтение объекта из потока ввода.
+         * @param is Входной поток.
+         */
         virtual void read(std::istream& is) = 0;
     };
 }
