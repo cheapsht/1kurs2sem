1fdc57756bf8c10aff60f587f9340ab9d90e0e3b
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-12 17:10:22 +0300

    figure.h

---
+++ include/figure.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+namespace graphics {
+    class Figure {
+    public:
+        virtual ~Figure() = default;
+        virtual std::string ToString() const = 0;
+        virtual void read(std::istream& is) = 0;
+    };
+}
