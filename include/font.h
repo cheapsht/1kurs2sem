0f4e2de7e694ac1f038f54d65d6a7724397a8f23
IZaparenchukTKI-142 <ilyazaparenchuk@mail.ru>
2026-05-12 17:12:20 +0300

    font.h

---
+++ include/font.h
@@ -0,0 +1,29 @@
+Lines changed: 31 additions & 0 deletions
+Original file line number	Original file line	Diff line number	Diff line change
+#pragma once
+#include <string>
+#include <iostream>
+#include "Color.h"
+namespace graphics {
+    /** @brief Класс Шрифт, хранит название, цвет и размер. */
+    class Font {
+    private:
+        std::string name;
+        Color color;
+        int size;
+    public:
+        /**
+         * @brief Конструктор шрифта.
+         * @throws std::invalid_argument Если размер <= 0.
+         */
+        Font(const std::string& name = "Arial", const Color& color = Color(),const  int size = 12);
+        const std::string& getName() const;
+        const Color& getColor() const;
+        int getSize() const;
+        void setName(const std::string& name);
+        void setColor(const Color& color);
+        void setSize(const int size);
+        friend std::ostream& operator<<(std::ostream& os, const Font& f);
+        friend std::istream& operator>>(std::istream& is, Font& f);
+    };
+}
